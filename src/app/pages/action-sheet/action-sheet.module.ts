779ef9f6f8b3c421860aa75f2a98b8684edbdab2
miguelorjuela45 <miguel-orjuelar@unilibre.edu.co>
2019-08-05 22:49:21 -0500

    Primer commit

---
+++ src/app/pages/action-sheet/action-sheet.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ActionSheetPage } from './action-sheet.page';
+import { HeaderComponent } from '../../componentes/header/header.component';
+import { ComponentesModule } from '../../componentes/componentes.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ActionSheetPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes),
+    ComponentesModule
+  ],
+  declarations: [ActionSheetPage]
+})
+export class ActionSheetPageModule {}
